db00781eaace3e0d24b612b305847f911f4f79fc
TJ Ness <82178149+tjness11@users.noreply.github.com>
2021-09-15 16:05:36 -0500

    Add files via upload

--- hedge-api/src/test/java/Game/data/KnownGoodState.java
+++ hedge-api/src/test/java/Game/data/KnownGoodState.java
@@ -1,4 +1,5 @@
 package Game.data;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,6 @@ public class KnownGoodState {
     static boolean hasRun = false;
 
     void set() {
-        if (!hasRun) {
-            hasRun = true;
             jdbcTemplate.update("call set_known_good_state();");
-        }
     }
 }
